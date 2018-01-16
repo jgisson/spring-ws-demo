604ea70e1a8823a9912fd1e08aebba8475add763
jgisson <jerome.gisson@gmail.com>
2018-01-16 11:41:14 +0100

    Spring Initializr code

---
+++ src/main/java/jgisson/spring/ws/springwsdemo/SpringWsDemoApplication.java
@@ -0,0 +1,12 @@
+package jgisson.spring.ws.springwsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWsDemoApplication.class, args);
+	}
+}
